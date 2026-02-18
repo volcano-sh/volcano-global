e368c42652836670f323b8efe20baf67f60f404b
goyalpalak18 <goyalpalak1806@gmail.com>
2026-02-18 10:08:10 +0530

    fix: prevent nil Locality panic in periodic DataSource update
    Signed-off-by: goyalpalak18 <goyalpalak1806@gmail.com>

--- pkg/controller/datadependency/plugin_manager.go
+++ pkg/controller/datadependency/plugin_manager.go
@@ -483,7 +483,14 @@ func (pm *PluginManager) updateSingleDataSource(ctx context.Context, ds *v1alpha
     }
 
 	// Update DataSource spec if clusters have changed
-	if !pm.clustersEqual(ds.Spec.Locality.ClusterNames, clusters) {
+	var currentClusters []string
+	if ds.Spec.Locality != nil {
+		currentClusters = ds.Spec.Locality.ClusterNames
+	}
+	if !pm.clustersEqual(currentClusters, clusters) {
+		if ds.Spec.Locality == nil {
+			ds.Spec.Locality = &v1alpha1.DataSourceLocality{}
+		}
 		ds.Spec.Locality.ClusterNames = clusters
 
 		_, err := pm.datadependencyClient.DatadependencyV1alpha1().DataSources().Update(ctx, ds, metav1.UpdateOptions{})
